8eab0b8538b6c1692d12839ecadbbb182bc59d7f
cloudphan <pdkhanh95@gmail.com>
2020-06-10 15:44:33 +0700

    added navBar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const navLinks = [
   {
@@ -25,7 +26,9 @@ export default function Navigation() {
       <span> My React App</span>
       <ul>
         {navLinks.map((link, index) => (
-          <li key={index}>{link.title}</li>
+          <li key={index}>
+            <Link to={link.path}>{link.title}</Link>
+          </li>
         ))}
       </ul>
     </nav>
