0d8a45b8e424c87346535a198abd11185ef87bba
cloudphan <pdkhanh95@gmail.com>
2020-06-13 23:17:18 +0700

    modified gitignore

---
+++ build/precache-manifest.d3a8913c7fa828a8767774249ff5c468.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ed3a63c7117cbdd3e3ac2eeecae691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "192202171ceb81c68293",
+    "url": "/static/css/2.495df28b.chunk.css"
+  },
+  {
+    "revision": "e870a72af069caf9b810",
+    "url": "/static/css/main.64fe1cd0.chunk.css"
+  },
+  {
+    "revision": "192202171ceb81c68293",
+    "url": "/static/js/2.222961fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.222961fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e870a72af069caf9b810",
+    "url": "/static/js/main.1025f0e8.chunk.js"
+  },
+  {
+    "revision": "04ce1151f7cc4e3803a1",
+    "url": "/static/js/runtime-main.a452570e.js"
+  },
+  {
+    "revision": "908fc04fa7ac9af70e8e916c8a8345d3",
+    "url": "/static/media/anyone_can_code.908fc04f.jpg"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/apollo_graphql.40913dfe.png"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/circuit_brain.40913dfe.jpg"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/cloud_storage.40913dfe.jpeg"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/cloud_storage.40913dfe.jpg"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/crypto.40913dfe.jpg"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/graphql.40913dfe.png"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/home_budget.40913dfe.jpg"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "/static/media/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "99b863497156d4478ec3489fefb3815c",
+    "url": "/static/media/ionicons.99b86349.woff"
+  },
+  {
+    "revision": "34a4f31c87f0e3f9e7c682ebf7eb3664",
+    "url": "/static/media/left-arrow.34a4f31c.svg"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/logo.40913dfe.png"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/money.40913dfe.jpg"
+  },
+  {
+    "revision": "4b236b7d6b99626604c0cbc84580a2ae",
+    "url": "/static/media/neuron.4b236b7d.jpg"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/programming_languages.40913dfe.jpeg"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/puzzled_programer.40913dfe.jpeg"
+  },
+  {
+    "revision": "5e5b72cc72ffd2c9ea04f8a4742dd6b1",
+    "url": "/static/media/right-arrow.5e5b72cc.svg"
+  },
+  {
+    "revision": "7ff06c676eedfaa89d8004893e0b6fe3",
+    "url": "/static/media/slide_home1.7ff06c67.jpg"
+  },
+  {
+    "revision": "40913dfe2a24d02a597068bde7ae92f1",
+    "url": "/static/media/spending_money.40913dfe.jpg"
+  }
+]);
